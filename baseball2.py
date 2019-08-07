4c15f12f9eed754df40140841d8c5c5de8f506d7
Jong-Woong <53574404+Jong-Woong@users.noreply.github.com>
2019-08-08 01:07:59 +0900

    Update baseball2.py

--- baseball2.py
+++ baseball2.py
@@ -1,18 +1,17 @@
 # baseball game
 import random
-random_num = random.sample(range(1, 10), 3)  #임의의 숫자 3개를 호출
-print(random_num)
+answer = list(random.sample(range(1, 10), 3))  #임의의 숫자 3개를 호출
 
+print(answer)
+
+print ('★☆★☆★☆★☆숫자야구게임★☆★☆★☆★☆\n{0:-^50}\n규칙 ▶ 숫자는 1~9까지 겹치지 않게 3개 입력한다'.format(''))
 try_ = 0
 strike = 0
 ball = 0
 
-print ('★☆★☆★☆★☆숫자야구게임★☆★☆★☆★☆\n{0:-^50}\n규칙 ▶ 숫자는 1~9까지 겹치지 않게 3개 입력한다'.format(''))
-
-#중복 숫자를 거르기 위한 수식
 while strike < 3:
     num = input('숫자를 입력하세요')
-
+    # 중복 숫자를 거르기 위한 수식
     if num[0] == num[1] == num[2]:
         print ('서로 다른 다시 숫자를 입력해주세요')
         continue
@@ -28,27 +27,18 @@ while strike < 3:
     else:
         pass
 
+    strike = 0
+    ball = 0
+
 #베이스볼 규칙
-while strike < 3
-for i in range(0,3):
+    for i in range(0,3):
+        for j in range(0,3):
+            if (num[i] == str(answer[j]) and i==j):
+                strike += 1
+            elif (num[i] == str(answer[j]) and i!=j):
+                ball += 1
     try_ += 1
-    for j in range(0,3):
-        if num[i] == num[j]:
-            strike += 1
+    print(try_,'회 결과 : ',strike, '스트라이크',ball,'볼','\n')
 
-        elif num[i] != num[j]:
-            ball += 1
-
-    if strike == 3:
-        print (str(strike),'스트라이크, 아웃!',str(try_),'만에 승리하셨습니다')
-        break
-    else:
-        print (str(try_),'회 게임 결과',str(strike),'스트라이크',str(ball)'볼')
-        print (try_+1,'게임을 다시 시작하세요')
-        strike = 0
-        ball = 0
-        continue
-
-
-
-
+print ('{0:-^50}'.format(''))
+print ('게임에서 승리하셨습니다')
